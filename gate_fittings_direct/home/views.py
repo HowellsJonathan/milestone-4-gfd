b3fe07a57123d7c61d358bfe32ef20c3146fd51b
Jon Howells <howells.jonnathan@gmail.com>
2021-02-28 14:06:11 +0000

    Added home templates and settings

--- gate_fittings_direct/home/views.py
+++ gate_fittings_direct/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Hello, Django!")
+def index(request):
+    """ A View to return Index page """
+    
+    return render(request, 'home/index.html')
