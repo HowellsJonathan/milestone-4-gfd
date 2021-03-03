b58087b584086943d57971893c102499ccfe1540
Jon Howells <howells.jonnathan@gmail.com>
2021-03-03 15:21:54 +0000

    Added models for fixtures and admin register

--- gate_fittings_direct/products/admin.py
+++ gate_fittings_direct/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, main_category, sub_category
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(main_category)
+admin.site.register(sub_category)
