efa825ae256156ced6d1f8b76343d8ff11df7716
Jon Howells <howells.jonnathan@gmail.com>
2021-03-04 18:27:36 +0000

    Added view, urls and html files for product detail

--- gate_fittings_direct/products/views.py
+++ gate_fittings_direct/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django_user_agents.utils import get_user_agent
 
@@ -19,3 +19,20 @@ def all_products(request):
         return render(request, 'products/mobile_products.html', context)
     elif user_agent.is_pc:
         return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A View to return a single products details """
+
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    user_agent = get_user_agent(request)
+
+    if user_agent.is_mobile or user_agent.is_tablet:
+        return render(request, 'products/mobile_product_detail.html', context)
+    elif user_agent.is_pc:
+        return render(request, 'products/product_detail.html', context)
