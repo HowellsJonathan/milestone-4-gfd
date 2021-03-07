2a025cddd128a015c8ebc9a0edbdcdbef850529d
Jon Howells <howells.jonnathan@gmail.com>
2021-03-07 19:48:27 +0000

    Added remove functionality to cart

--- gate_fittings_direct/bag/views.py
+++ gate_fittings_direct/bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -43,8 +43,28 @@ def adjust_bag(request, item_id):
     if quantity > 0:
         bag[item_id] = quantity
     else:
-        bag.pop[item_id]
+        bag.pop(item_id)
 
     # overright bag variable for session
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove an item from the bag """
+
+    try:
+        # redirect url for when item has been added to bag, to redirect
+        # back to same page
+        redirect_url = request.POST.get('redirect_url')
+        # create or get a bag from the session for the user
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+
+        # overright bag variable for session
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+        
+    except Exception as e:
+        return HttpResponse(status=500)
