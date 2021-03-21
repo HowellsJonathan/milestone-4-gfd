e257256719f020145f222c3d53aa5f4fcdde300d
Jon Howells <howells.jonnathan@gmail.com>
2021-03-21 13:06:35 +0000

    Added forms.py to customise form

---
+++ gate_fittings_direct/checkout/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs) # Set form up as default before customization
+        placeholders = { # Dict of placeholder instead of labels etc.
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True # Cursor start in Full Name field
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder # Set all placeholder attr to above
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False # Remove Labels
