55bda4705af2d91f746563d48c19a2315ad53623
Jon Howells <howells.jonnathan@gmail.com>
2021-03-21 11:23:11 +0000

    Added other toasts to bag views for remove, update etc.

--- gate_fittings_direct/bag/views.py
+++ gate_fittings_direct/bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,7 +14,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add an item to a bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     # redirect url for when item has been added to bag, to redirect
     # back to same page
@@ -25,10 +25,11 @@ def add_to_bag(request, item_id):
     if item_id in list(bag.keys()):
         # update quantity of item if already in bag
         bag[item_id] += quantity
+        messages.success(request, f'Updated { product.name } quantity to {bag[item_id]}')
     else:
         # add item to bag
-        messages.success(request, f'Added { product.name } to your bag')
         bag[item_id] = quantity
+        messages.success(request, f'Added { product.name } to your bag')
 
     # overright bag variable for session
     request.session['bag'] = bag
@@ -38,17 +39,20 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust an items quantity in the bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     # redirect url for when item has been added to bag, to redirect
     # back to same page
     redirect_url = request.POST.get('redirect_url')
     # create or get a bag from the session for the user
     bag = request.session.get('bag', {})
-
+    
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated { product.name } quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed { product.name } from your bag')
 
     # overright bag variable for session
     request.session['bag'] = bag
@@ -59,6 +63,7 @@ def remove_from_bag(request, item_id):
     """ Remove an item from the bag """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         # redirect url for when item has been added to bag, to redirect
         # back to same page
         redirect_url = request.POST.get('redirect_url')
@@ -66,10 +71,12 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
+        messages.success(request, f'Removed { product.name } from your bag')
 
         # overright bag variable for session
         request.session['bag'] = bag
         return HttpResponse(status=200)
         
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
