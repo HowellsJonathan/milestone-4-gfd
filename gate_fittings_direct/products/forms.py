22f851d35dcbf06cc84d26b36ae6c56c3a16678a
Jon Howells <howells.jonnathan@gmail.com>
2021-03-27 17:47:20 +0000

    Created forms.py for adding products

---
+++ gate_fittings_direct/products/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        main_categories = main_category.objects.all()
+        sub_categories = sub_categories.objects.all()
+        main_friendly_names = [(c.id, c.get_friendly_name()) for c in main_categories]
+        sub_friendly_names = [(c.id, c.get_friendly_name()) for c in sub_categories]
+
+        self.fields['main_category'].choices = main_friendly_names
+        self.fields['sub_category'].choices = sub_friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
