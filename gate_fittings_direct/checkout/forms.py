6d0a6077099776cb6647ffc737e15d414ce17c63
Jon Howells <howells.jonnathan@gmail.com>
2021-03-28 19:30:42 +0100

    Styled checkout for desktop

--- gate_fittings_direct/checkout/forms.py
+++ gate_fittings_direct/checkout/forms.py
@@ -22,10 +22,19 @@ class OrderForm(forms.ModelForm):
             'phone_number': 'Phone Number',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
-            'street_address1': 'Street Address 1',
-            'street_address2': 'Street Address 2',
+            'street_address1': 'Address',
+            'street_address2': 'Address 2',
             'county': 'County',
         }
+        
+        self.fields['full_name'].label = "Full Name:"
+        self.fields['email'].label = "Email:"
+        self.fields['phone_number'].label = "Phone Number:"
+        self.fields['postcode'].label = "Postcode:"
+        self.fields['town_or_city'].label = "Town / City:"
+        self.fields['street_address1'].label = "Address"
+        self.fields['street_address2'].label = "Address 2 <small class='text-muted'>(optional)</small>"
+        self.fields['county'].label = "County"
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
@@ -36,4 +45,3 @@ class OrderForm(forms.ModelForm):
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
