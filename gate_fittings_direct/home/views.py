720c3a40dda74361afa1673b079ff4ffc6588436
Jon Howells <howells.jonnathan@gmail.com>
2021-03-28 20:31:29 +0100

    Started styling mobile index page, using desk and responsive

--- gate_fittings_direct/home/views.py
+++ gate_fittings_direct/home/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django_user_agents.utils import get_user_agent
 
 def index(request):
     """ A View to return Index page """
 
-    user_agent = get_user_agent(request)
-    if user_agent.is_mobile or user_agent.is_tablet:
-        return render(request, 'home/mobile_index.html')
-    elif user_agent.is_pc:
-        return render(request, 'home/index.html')
+    return render(request, 'home/index.html')
