3c06faa176f29a7c2ed298394397bad996b780e5
Jon Howells <howells.jonnathan@gmail.com>
2021-03-30 19:30:12 +0100

    Created URL tests for products and profiles

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import (
+    all_products, product_detail, add_product, edit_product, delete_product
+)
 
-# Create your tests here.
+""" Url Testing """
+
+class TestUrls(SimpleTestCase):
+
+    def test_all_products_resolves(self):
+        url = reverse('products')
+        self.assertEqual(resolve(url).func, all_products)
+
+    def test_product_detail_resolves(self):
+        url = reverse('product_detail', args='1')
+        self.assertEqual(resolve(url).func, product_detail)
+
+    def test_add_product_resolves(self):
+        url = reverse('add_product')
+        self.assertEqual(resolve(url).func, add_product)
+
+    def test_edit_product_resolves(self):
+        url = reverse('edit_product', args='1')
+        self.assertEqual(resolve(url).func, edit_product)
+
+    def test_delete_product_resolves(self):
+        url = reverse('delete_product', args='1')
+        self.assertEqual(resolve(url).func, delete_product)
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import profile, order_history
 
-# Create your tests here.
+""" Url Testing """
+
+class TestUrls(SimpleTestCase):
+
+    def test_profile_resolves(self):
+        url = reverse('profile')
+        self.assertEqual(resolve(url).func, profile)
+
+    def test_order_history_resolves(self):
+        url = reverse('order_history', args='1')
+        self.assertEqual(resolve(url).func, order_history)
+
