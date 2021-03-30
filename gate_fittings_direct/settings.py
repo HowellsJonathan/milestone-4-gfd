3c9610bb74cef6b6f916bd95291c249f285df031
Jon Howells <howells.jonnathan@gmail.com>
2021-03-30 14:17:06 +0100

    Added email settings for live emails

--- gate_fittings_direct/settings.py
+++ gate_fittings_direct/settings.py
@@ -110,8 +110,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email' 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -216,14 +214,20 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-DEFAULT_FROM_EMAIL = 'info@gfd4trade.co.uk'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'info@gfd4trade.co.uk'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.ionos.com'
+    EMAIL_PORT = 487
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
-""" EMAIL_HOST = 'smtp.ionos.com'
-EMAIL_PORT = 487
-EMAIL_HOST_USER = 'info@gfd4trade.co.uk'
-EMAIL_HOST_PASSWORD = 'TheAdd!ct66'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True """
+
 
 # User agents
 
