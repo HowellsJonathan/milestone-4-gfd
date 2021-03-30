9f70ee52d58ad929f8fc2157d32809063fa93119
Jon Howells <howells.jonnathan@gmail.com>
2021-03-30 12:12:22 +0100

    Removed secret key and set debug

--- gate_fittings_direct/settings.py
+++ gate_fittings_direct/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$-v!k5bdf9%f7wu^%_dq#%mrphigg9+09^fld21^hgf787wt+('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gfd4trade.herokuapp.com', 'localhost', '127.0.0.1']
 
